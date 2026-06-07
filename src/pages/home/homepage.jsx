ec4be3275f5d719b19149fd217f1531a2ebb9ac8
Kobus <kobus@orbtech.co.za>
2026-06-07 13:23:51 +0200

    I do not really no

--- src/pages/home/homepage.jsx
+++ src/pages/home/homepage.jsx
@@ -1,6 +1,6 @@
 import TopBanner from './sections/top-banner/top-banner.jsx';
 import ProblemSection from './sections/problem/problem-section.jsx';
-import SolutionSection from './sections/solution/solution.jsx';
+import SolutionSection from './sections/solution/solution-section.jsx';
 import './home.css';
 
 const HomePage = () => {

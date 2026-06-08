56fffd7a6a24fe7512dbda824183ca8a443f636b
Kobus <kobus@orbtech.co.za>
2026-06-08 23:58:57 +0200

    Updated 'howitworks' experience

--- src/pages/howitworks/sections/top-banner/top-banner.jsx
+++ src/pages/howitworks/sections/top-banner/top-banner.jsx
@@ -1,11 +1,15 @@
 import "./top-banner.css"
+import Heading from "../../../../components/heading/heading"
 
 export default function TopBanner() {
     return(
-        <section className="top-section">
-                <p>HOW IT WORKS</p>
-                <h1>A structured process, built for the long term.</h1>
-                <h3>From brief to placement to monthly support — here's exactly what working with BrightHire looks like.</h3>
+        <section className="banner-primary">
+            <Heading 
+                smallText="How it works"
+                heading="A structured process, built for the long term." 
+                subText="From brief to placement to monthly support — here's exactly what working with BrightHire looks like."
+                className="heading-white subtext-white"
+            />
         </section>
     )
 }

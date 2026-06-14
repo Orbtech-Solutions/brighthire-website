5556dbc8b46d99a98f72af49bd3e247837cb1bda
Kobus <kobus@orbtech.co.za>
2026-06-14 23:15:35 +0200

    Centered /how-it-works/partnership-section

--- src/pages/howitworks/sections/partnership/partnership.jsx
+++ src/pages/howitworks/sections/partnership/partnership.jsx
@@ -4,12 +4,15 @@ import Heading from "../../../../components/heading/heading.jsx"
 
 export default function Partnership() {
     return(
-        <section className="whole-section">
-            <div className="section-inner section-partnership">
-                <Card heading={<Heading smallText="In partnership with" heading="Work 4 A Living"/>} body="W4AL is a South African non-profit transforming lives through mentorship, skills development and biblical values. 
+        <section className="partnership-section">
+            <div className="section-inner">
+                <Card 
+                heading={<Heading smallText="In partnership with" heading="Work 4 A Living"/>} 
+                body="W4AL is a South African non-profit transforming lives through mentorship, skills development and biblical values. 
                 Beyond the Job Excellence Course, W4AL offers 30+ additional courses and development programmes, allowing candidates to specialise and become even more valuable employees. 
                 BrightHire works hand in hand with W4AL facilitators nationwide to connect businesses with people who are dependable, motivated and ready to contribute positively from day one." 
-                className="section-partnership__card"/>
+                className="section-partnership__card"
+                />
             </div>
         </section>
     )
